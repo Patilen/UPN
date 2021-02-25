1aa35ed56c91644a77bab31e73557e593d8a4d8f
Tilen <40770099+TilenG5@users.noreply.github.com>
2021-02-25 18:12:06 +0100

    Add files via upload

---
+++ Praksa/Vaja_09/Program1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaja9
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vnesi število: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Vnesi število: ");
+            int y = int.Parse(Console.ReadLine());
+
+            if(x < y)
+            {
+                Console.WriteLine(y);
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+            }
+            Console.ReadLine();
+        }
+    }
+}
