d54138cca3f3cc2dd9d4ff2ee059755c3076dcb1
echojg <79456867+echojg@users.noreply.github.com>
2021-02-26 13:33:38 +0100

    Create Naloga_4.cs

---
+++ Praksa/Naloga_4.cs
@@ -0,0 +1,33 @@
+// Napiši program, ki glede na vpisan znak izpiše spol (m,M - moski, z,Z - zenski, za vse ostale pa "vpisali ste napacen spol"). Nalogo reši z uporabo switch-case.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naloga_4
+{
+    class Program4
+    {
+        static void Main(string[] args) // By Jon-Nik Gorenak, R-2.b
+        {
+            Console.Write("Vnesi spol: ");
+            char spol = Convert.ToChar(Console.ReadLine());
+
+            switch (spol)
+            {
+                case 'm':
+                    Console.WriteLine("M-Moski");
+                    break;
+                case 'z':
+                    Console.WriteLine("Z-zenski");
+                    break;
+                default:
+                    Console.WriteLine("Vpisali ste napacen spol");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
