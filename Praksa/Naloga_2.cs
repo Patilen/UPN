253e7bb841e94a77ac9c73fed3c5f6e11c622a5f
echojg <79456867+echojg@users.noreply.github.com>
2021-02-26 13:29:41 +0100

    Create Naloga_2.cs

---
+++ Praksa/Naloga_2.cs
@@ -0,0 +1,39 @@
+// Napiši program, ki glede na vpisan znak izpiše barvo (r,R - rdeča, m,M - modra, z,Z - zelena, b,B - bela, za vse ostale pa črna).Nalogo reši z uporabo switch-case. //
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naloga_2
+{
+    class Program2
+    {
+        static void Main(string[] args)  // By Jon-Nik Gorenak
+        {
+            Console.WriteLine("Vnesi ime barve:");
+            char barva = Convert.ToChar(Console.ReadLine());
+
+            switch (barva)
+            {
+                case 'r':
+                    Console.WriteLine("Rdeča");
+                    break;
+                case 'm':
+                    Console.WriteLine("Modra");
+                    break;
+                case 'z':
+                    Console.WriteLine("Zelena");
+                    break;
+                case 'b':
+                    Console.WriteLine("Bela");
+                    break;
+                default:
+                    Console.WriteLine("Črna");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
