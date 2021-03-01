d51d6879e54266121e8cb72868893e0c2a56d386
Tilen <40770099+TilenG5@users.noreply.github.com>
2021-03-01 17:26:25 +0100

    Add files via upload

---
+++ Praksa/Vaja_12/Program4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naloga4
+{
+    class Program4
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vnesi število: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Vnesi število: ");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                do
+                {
+                    y++;
+                    Console.WriteLine(y);
+                } while (y < x - 1);
+            }
+            else
+            {
+                do
+                {
+                    x++;
+                    Console.WriteLine(x);
+                } while (y - 1 > x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
