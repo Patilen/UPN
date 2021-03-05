e2aa81a6334366bd51b62122d9766a5c6b20f753
patik123 <patrick.kosir@gmail.com>
2021-03-05 16:22:22 +0100

    Dodajanje vaje 11 pri praktičnem pouku

---
+++ Praksa/Vaja_11/Program3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naloga3
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (x != 0)
+            {
+                Console.Write("Vpiši število: ");
+                x = int.Parse(Console.ReadLine());
+
+                if (x != 0)
+                {
+                    Console.WriteLine("Koda za {0} je {1}", x, (char)x);
+                }
+            }
+            Console.WriteLine("KONEC!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Praksa/Vaja_11/Program5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naloga5
+{
+    class Program5
+    {
+        static void Main(string[] args)
+        {
+            float rezultatpoz = 0;
+            float rezultatneg = 0;
+
+            int i = 0;
+            while (i < 20)
+            {
+                Console.Write("Vnesi število: ");
+                float x = float.Parse(Console.ReadLine());
+
+                if (x > 0)
+                {
+                    rezultatpoz += x;
+                }
+                else
+                {
+                    rezultatneg += x;
+                }
+                i++;
+            }
+            Console.WriteLine("Vsota pozitivnih števil je {0} , vsota negativnih števil je {1}", rezultatpoz, rezultatneg);
+            Console.ReadLine();
+        }
+    }
+}
