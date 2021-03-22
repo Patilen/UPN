106023093b14d19977300f6083cb61af620f00dd
patik123 <patrick.kosir@gmail.com>
2021-03-22 12:41:33 +0100

    Add 12

---
+++ Praksa/Vaja_12/Program2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naloga2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            string ime = "";
+            do
+            {
+                Console.Write("Vnesi ime: ");
+                ime = Console.ReadLine();
+
+                if (ime != "KONEC")
+                {
+                    i++;
+                }
+            } while (ime != "KONEC");
+            Console.WriteLine("Vnešeno število imen: {0}", i);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Praksa/Vaja_12/Program7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naloga7
+{
+    class Program7
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vpiši začetek intervala: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Vpiši konec intervala: ");
+            int y = int.Parse(Console.ReadLine());
+
+            int rezultat = 0;
+            do
+            {
+                rezultat += x;
+                x++;
+            } while (x <= y);
+            Console.WriteLine("Vsota vseh števil v tem intervalu je {0}", rezultat);
+            Console.ReadLine();
+        }
+    }
+}
